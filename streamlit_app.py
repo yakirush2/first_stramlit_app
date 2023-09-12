871024f921db58a97e617f610934699a57f65852
yakirush2 <144795579+yakirush2@users.noreply.github.com>
2023-09-12 14:44:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Our diner offers a variety of healthy food options.')
 
-streamlit.text (    'Salad': 'Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.')
+streamlit.text (    'Salad:' 'Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.')
 
 
