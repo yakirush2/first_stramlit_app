306dd1b0a1e79f7911c30f12bbb1c678becd9fb7
yakirush2 <144795579+yakirush2@users.noreply.github.com>
2023-09-12 14:31:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 
-streamlit.write('Our diner offers a variety of healthy food options.')
+streamlit.write ('Our diner offers a variety of healthy food options.')
 
 
 
