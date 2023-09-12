bf3112752cf1256f1bb11769e0056c936ff3a1e0
yakirush2 <144795579+yakirush2@users.noreply.github.com>
2023-09-12 14:43:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Our diner offers a variety of healthy food options.')
 
-streamlit.write ("Salad:" "Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.",
-    "\nGrilled Chicken Breast:" "Tender grilled chicken breast with a side of steamed vegetables.",
-    "\nQuinoa Bowl:" "A nutritious bowl with quinoa, mixed vegetables, and a tahini sauce.",
-    "\nFruit Salad:" "A medley of seasonal fruits for a refreshing dessert.",
-    "\nGreen Smoothie:" "A healthy blend of spinach, banana, and almond milk.",)
+streamlit.text (    'Salad': 'Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.')
 
 
