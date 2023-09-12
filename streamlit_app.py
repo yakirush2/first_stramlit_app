5c27ffaf52f2ab113442d562e554f8b93871605f
yakirush2 <144795579+yakirush2@users.noreply.github.com>
2023-09-12 14:46:53 +0300

    Update streamlit_app.py
    latest change, adding menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Our diner offers a variety of healthy food options.')
 
-streamlit.text (    'Salad:' 'Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.')
+streamlit.text ('Salad:' 'Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.')
+streamlit.text ('Grilled Chicken Breast:' 'Tender grilled chicken breast with a side of steamed vegetables.')
+streamlit.text ('Quinoa Bowl:' 'A nutritious bowl with quinoa, mixed vegetables, and a tahini sauce.')
+streamlit.text ('Fruit Salad:' 'A medley of seasonal fruits for a refreshing dessert.')
 
 
