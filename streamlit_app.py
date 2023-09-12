6407e38c75957f08422dac69c46e3a37d6420a17
yakirush2 <144795579+yakirush2@users.noreply.github.com>
2023-09-12 14:29:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,7 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 
-# Define the menu items and their descriptions
-menu = {
-    "Salad": "Fresh greens, tomatoes, cucumbers, and a balsamic vinaigrette dressing.",
-    "Grilled Chicken Breast": "Tender grilled chicken breast with a side of steamed vegetables.",
-    "Quinoa Bowl": "A nutritious bowl with quinoa, mixed vegetables, and a tahini sauce.",
-    "Fruit Salad": "A medley of seasonal fruits for a refreshing dessert.",
-    "Green Smoothie": "A healthy blend of spinach, banana, and almond milk.",
-}
+st.write('Our diner offers a variety of healthy food options.')
 
-# Display the menu using a loop
-for item, description in menu.items():
-    st.subheader(item)
-    st.write(description)
+
 
